fd4dcca16397adcd01a1702ba78fbb34eecb7ca3
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-15 11:31:47 +0200

    Add basic nav element

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from "./sections/Navbar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="relative w-screen min-h-screen overflow-x-hidden">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
