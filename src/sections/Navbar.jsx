8b1c45f053340c4303fb27924a639bc264f0bf34
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-17 12:48:28 +0200

    Use react scroll component instead of <a></a>

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,6 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { socials } from "../constants";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Link } from "react-scroll";
 
 function Navbar() {
   const navRef = useRef(null);
@@ -111,12 +112,18 @@ function Navbar() {
         className="fixed z-50 flex flex-col justify-between w-full h-full px-10 uppercase bg-black text-white/80 py-28 gap-y-10 md:w-1/2 md:left-1/2"
       >
         <div className="flex flex-col text-5xl gap-y-2 md:text-6xl lg:text-8xl">
-          {["home", "services", "about", "work", "contact"].map(
+          {["home", "skills", "about", "work", "contact"].map(
             (section, index) => (
               <div key={index} ref={(el) => (linksRef.current[index] = el)}>
-                <a className="transition-all duration-300 cursor-pointer hover:text-white">
+                <Link
+                  to={`${section}`}
+                  className="transition-all duration-300 cursor-pointer hover:text-white"
+                  smooth
+                  offset={0}
+                  duration={2000}
+                >
                   {section}
-                </a>
+                </Link>
               </div>
             )
           )}
