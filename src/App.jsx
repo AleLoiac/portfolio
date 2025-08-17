6d17c22473897072ea832e6dbd9b30aa489a2c62
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-17 15:00:52 +0200

    Add hero component and initial implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 
 function App() {
   return (
     <div className="relative w-screen min-h-screen overflow-x-hidden">
       <Navbar />
+      <Hero />
     </div>
   );
 }
