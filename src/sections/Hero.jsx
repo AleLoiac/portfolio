045fac8ac6003c08ecba8241a856430b682fd52e
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-18 15:22:13 +0200

    Add Planet to the hero section and animate it

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,14 @@ import { useRef } from "react";
 import AnimatedTextLines from "../components/AnimatedTextLines";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import { Canvas } from "@react-three/fiber";
+import { Planet } from "../components/Planet";
+import { Environment, Float, Lightformer } from "@react-three/drei";
+import { AmbientLight } from "three";
+import { useMediaQuery } from "react-responsive";
 
 function Hero() {
+  const isMobile = useMediaQuery({ maxWidth: 853 });
   const contextRef = useRef(null);
   const headerRef = useRef(null);
   const aboutText = `I build modern, performant websites and front-end interfaces
@@ -59,6 +65,49 @@ function Hero() {
           </div>
         </div>
       </div>
+      <figure
+        className="absolute inset-0 -z-50"
+        style={{ width: "100vw", height: "100vh" }}
+      >
+        <Canvas
+          shadows
+          camera={{ position: [0, 0, -10], fov: 17.5, near: 1, far: 20 }}
+        >
+          <ambientLight intensity={0.5} />
+          <Float speed={0.5}>
+            <Planet scale={isMobile ? 0.7 : 1} />
+          </Float>
+          <Environment resolution={256}>
+            <group rotation={[-Math.PI / 3, 4, 1]}>
+              {/* add illumination */}
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 5, -9]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[0, 3, 1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[5, -1, -1]}
+                scale={10}
+              />
+              <Lightformer
+                form={"circle"}
+                intensity={2}
+                position={[10, 1, 0]}
+                scale={16}
+              />
+            </group>
+          </Environment>
+        </Canvas>
+      </figure>
     </section>
   );
 }
