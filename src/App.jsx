b48df25dc6f17fa10382f820d2cfa7637a4f12ee
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-19 21:08:13 +0200

    Finish and animate Skills sections
    Added in the commit the Lenis import and used it
    to improve scrolling smoothness

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import ReactLenis from "lenis/react";
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import Skills from "./sections/Skills";
@@ -5,12 +6,16 @@ import SkillSummary from "./sections/SkillSummary";
 
 function App() {
   return (
-    <div className="relative w-screen min-h-screen overflow-x-hidden">
+    // Lenis just makes the scrolling smoother
+    <ReactLenis
+      root
+      className="relative w-screen min-h-screen overflow-x-hidden"
+    >
       <Navbar />
       <Hero />
       <SkillSummary />
       <Skills />
-    </div>
+    </ReactLenis>
   );
 }
 
