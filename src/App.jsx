c52e36e5e93a07b1e2b053248ac07ca3060e59cb
AleLoiac <107403992+AleLoiac@users.noreply.github.com>
2025-08-19 14:41:08 +0200

    Add an animated skill summary section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
+import SkillSummary from "./sections/SkillSummary";
 
 function App() {
   return (
     <div className="relative w-screen min-h-screen overflow-x-hidden">
       <Navbar />
       <Hero />
+      <SkillSummary />
     </div>
   );
 }
